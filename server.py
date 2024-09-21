554875296969f79461fe97f7694d87384b6640d9
Nathnael Tamirat <nathnaeltamirat3@gmail.com>
2024-09-21 10:09:33 -0400

    edited server.py

--- server.py
+++ server.py
@@ -1,22 +1,28 @@
+'''This is Ai developed flask application using watson API'''
 from flask import Flask,request,render_template
-import requests
-from EmotionDetection.emotion_detection import emotion_detector 
+from EmotionDetection.emotion_detection import emotion_detector
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    return template
+    """
     return render_template("index.html")
 @app.route("/emotionDetector")
 def emotion_detector_route():
+    """
+    return emotion values and dominant emotion
+    """
     text = request.args.get("textToAnalyze")
-    if text == None:
+    if text is None:
         return "Invalid text! Please try again!.\n"
     emotion = emotion_detector(text)
     prevalue = "For the given statement, the system response is "
-    emotions = f"'anger': {emotion['anger']}, 'disgust': {emotion['disgust']},'fear': {emotion['fear']}, 'joy': {emotion['joy']}, 'sadness': {emotion['sadness']}"
+    emotions = ', '.join(
+        f"'{key}': {emotion[key]}" for key in ['anger', 'disgust', 'fear', 'joy', 'sadness']
+    )
     dominant_emotion = f"The dominant emotion is {emotion['dominant_emotion']}."
- 
-        
     return f"{prevalue}{emotions} {dominant_emotion} \n"
 if __name__ == "__main__":
     app.run(debug=True)
